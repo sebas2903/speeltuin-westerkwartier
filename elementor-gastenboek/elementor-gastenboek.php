e50b4d85e70535d7e0a5d79e9345cdf8ecefb0b7
sebas2903 <61698750+sebas2903@users.noreply.github.com>
2020-12-10 14:18:34 +0100

    plugin toevoegen
    hier voeg ik alleen nog maar de plugin toen. zelf doet dit niks alleen maar test laten zien wanneer je hem in elementor sleept.

---
+++ elementor-gastenboek/elementor-gastenboek.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Elementor Gastenboek WordPress Plugin
+ *
+ * @package ElementorGastenboek
+ *
+ * Plugin Name: Elementor Speeltuinwesterkwartier Gastenboek
+ * Description: Gastenboek voor de gastenboek widget
+ * Version:     1.0.0
+ * Author:      Sebastiaan Heerema
+ Text Domain: elementor-gastenboek
+ */
+ define( 'ELEMENTOR_GASTENBOEK', __FILE__ );
+
+/**
+ * Include the Elementor_Awesomesauce class.
+ */
+ require plugin_dir_path( ELEMENTOR_GASTENBOEK ) . 'class-elementor-gastenboek.php';
---
+++ elementor-gastenboek/widgets/class-gastenboek.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Gastenboek class.
+ *
+ * @category   Class
+ * @package    ElementorGastenboek
+ * @subpackage WordPress
+ * @author     Sebastiaan Heerema
+ * @copyright  2020 Sebastiaan Heerema
+ * @since      1.0.0
+ * php version 7.3.9
+ */
+
+namespace ElementorGastenboek\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Scheme_Typography;
+
+// Security Note: Blocks direct access to the plugin PHP files.
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Awesomesauce widget class.
+ *
+ * @since 1.0.0
+ */
+class Gastenboek extends Widget_Base {
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $data Widget data.
+	 * @param array $args Widget arguments.
+	 */
+	public function __construct( $data = array(), $args = null ) {
+		parent::__construct( $data, $args );
+	}
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'gastenboek';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Gastenboek', 'elementor-gastenboek' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'fa fa-pencil';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return array( 'general' );
+	}
+	
+	/**
+	 * Enqueue styles.
+	 */
+	public function get_style_depends() {
+		return array( 'gastenboek' );
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			array(
+				'label' => __( 'Content', 'elementor-gastenboek' ),
+			)
+		);
+
+        $this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+        
+        ?>
+
+        <?php
+        echo "<p style=color:black;>test</p>";
+        require('dbconnect.php');
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _content_template() {
+        echo "<p style=color:black;>test</p>";
+            
+	}
+}
