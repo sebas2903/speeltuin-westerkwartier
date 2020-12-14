e4642a3a7a1e100819a07c5b359667cb44d965f3
sebas2903 <61698750+sebas2903@users.noreply.github.com>
2020-12-14 17:10:43 +0100

    datum type veranderen

--- elementor-gastenboek/widgets/class-gastenboek.php
+++ elementor-gastenboek/widgets/class-gastenboek.php
@@ -151,8 +151,7 @@ class Gastenboek extends Widget_Base {
                         $bericht = safe($_POST['bericht']);
                         $bericht = $conn->real_escape_string($bericht);
                         $today = date('Y-m-d H:i:s');
-                        date_default_timezone_set('Europe/Amsterdam');
-                        $todaycorrecttimezone = ('d.m.Y, H:i:s');
+                        $todaycorrecttimezone = ('d.m.Y');
                         
                         $sql = "INSERT INTO mFD13_comments
                         (comment_ID, comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_date_gmt, comment_content, comment_karma, comment_approved, comment_agent, comment_type, comment_parent, user_id) 
