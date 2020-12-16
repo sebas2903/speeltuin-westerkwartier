b755f97e935fa17e2b63874d5a0b900ebbecb88d
sebas2903 <61698750+sebas2903@users.noreply.github.com>
2020-12-16 16:04:26 +0100

    toch even iets anders proberen met pathfile

--- elementor-nieuwsbrief/widgets/class-nieuwsbrief.php
+++ elementor-nieuwsbrief/widgets/class-nieuwsbrief.php
@@ -21,7 +21,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 
-require '/../../elementor-gastenboek/widgets/vendor/autoload.php';
+require 'autoload.php';
 
 // Security Note: Blocks direct access to the plugin PHP files.
 defined( 'ABSPATH' ) || die();
