e9a0c666497f72f35e0136ef54c2bf215ce52478
sebas2903 <61698750+sebas2903@users.noreply.github.com>
2020-12-16 15:41:31 +0100

    nieuwe plugin template toevoegen zodat ik er straks in kan gaan werken.

---
+++ elementor-nieuwsbrief/widgets/class-nieuwsbrief.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * Nieuwsbrief class.
+ *
+ * @category   Class
+ * @package    ElementorNieuwsbrief
+ * @subpackage WordPress
+ * @author     Sebastiaan Heerema
+ * @copyright  2020 Sebastiaan Heerema
+ * @since      1.0.0
+ * php version 7.3.9
+ */
+namespace ElementorNieuwsbrief\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+use Elementor\Scheme_Typography;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'vendor/autoload.php';
+
+// Security Note: Blocks direct access to the plugin PHP files.
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Awesomesauce widget class.
+ *
+ * @since 1.0.0
+ */
+class Nieuwsbrief extends Widget_Base {
+	/**
+	 * Class constructor.
+	 *
+	 * @param array $data Widget data.
+	 * @param array $args Widget arguments.
+	 */
+	public function __construct( $data = array(), $args = null ) {
+		parent::__construct( $data, $args );
+	}
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'nieuwsbrief';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Nieuwsbrief', 'elementor-nieuwsbrief' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'fa fa-pencil';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return array( 'general' );
+	}
+	
+	/**
+	 * Enqueue styles.
+	 */
+	public function get_style_depends() {
+		return array( 'nieuwsbrief' );
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			array(
+				'label' => __( 'Content', 'elementor-nieuwsbrief' ),
+			)
+		);
+
+        $this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+    }
+        ?>
+        <form method=post style="color:black;">
+
+        </form>
+        <?php
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _content_template() {
+        ?>
+        <form method=post style="color:black;">
+
+        </form>
+        <?php
+        require('dbconnect.php');
+            
+	}
+}
+
