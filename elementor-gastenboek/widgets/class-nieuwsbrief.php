0624a6a0064000e9a18eed1ce979d6801de2d63e
sebas2903 <61698750+sebas2903@users.noreply.github.com>
2020-12-17 12:12:32 +0100

    weer css aanpassen

--- elementor-gastenboek/widgets/class-nieuwsbrief.php
+++ elementor-gastenboek/widgets/class-nieuwsbrief.php
@@ -191,7 +191,7 @@ class Nieuwsbrief extends Widget_Base {
         }
         ?>
         <form method=post style="color:white;">
-            <label style='font-size:2.5rem; color:black;'>Abboneer op onze nieuwsbrief</label><input style="border-radius:10px; font-size:2rem;"  type=email name=email value=email/>
+            <label style='font-size:2.5rem; color:white;'>Abboneer op onze nieuwsbrief</label><input style="border-radius:10px; font-size:2rem;"  type=email name=email placeholder=email/>
             <input type=submit name=add style="border-radius:10px; color:black; background-color:white; font-size:2rem; width:100%; margin-top:1vh;" value="Inschrijven"/>
             <?php echo $error; ?>
         </form>
